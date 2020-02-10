5864be9e3afce126d51bed6e923060ed41526487
Tarang Hirani <tarang9211@gmail.com>
2020-02-10 12:01:01 +1100

    added cypress example

---
+++ apps/next-cypress/cypress/integration/examples/monorepo.spec.js
@@ -0,0 +1,5 @@
+describe('Basic Test', () => {
+  it('Does not do much!', function() {
+    cy.visit('http://localhost:3000/');
+  })
+})
---
+++ apps/next-cypress/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hello = () => {
+  return <div>Hello</div>
+};
+
+export default Hello;
